48096e9615322aa47a2a936fd8cf004238efe5eb
Hugo Guay <dancinoman@gmail.com>
2024-12-15 11:11:39 -0500

    improved chat box

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 
 import streamlit as st
 import model_run as mr
+import time
 
 def build_main_page():
     """
     Streamlit app entry point.
     """
+
+    def use_chat_box(user_reply_and_wait):
+
+        if user_reply_and_wait:
+            for i, message in enumerate(st.session_state['chat_history']):
+                if i != len(st.session_state['chat_history']):
+                    st.markdown(message['message'])
+
+        else:
+            st.markdown(st.session_state['chat_history'][-1]['message'])
 
     # Top of the page
     st.title("Document Question Answering")
@@ -21,7 +32,7 @@ def build_main_page():
     uploaded_file = st.file_uploader("Upload Contract PDF", type="pdf")
 
     # Manage the reset state for new uploaded PDF
-    if "chat_history"in st.session_state:
+    if "chat_history" in st.session_state:
         if st.session_state["chat_history"] != [] and uploaded_file == None:
 
             # Convert message structure to a text file
@@ -41,17 +52,21 @@ def build_main_page():
     if uploaded_file:
         document_text = mr.get_from_pdf(uploaded_file)
 
-        if user_input := st.chat_input('Ask your question'):
-            user_input = user_input.strip()
+        # If there is document valid, user input is available
+        if document_text:
 
-            if document_text:
-                answer = mr.process_and_answer(user_input, document_text)
-
+            # User input and chatbox mangament
+            if user_input := st.chat_input('Ask your question'):
+                user_input = user_input.strip()
                 st.session_state.chat_history.append({'message': f'**You**: {user_input}'})
-                st.session_state.chat_history.append({'message':f"**AI**:'{answer}'"})
-                for message in st.session_state['chat_history']:
-                    st.markdown(message['message'])
 
+                use_chat_box(True)
+
+                with st.spinner('Reading'):
+                    answer = mr.process_and_answer(user_input, document_text)
+
+                st.session_state.chat_history.append({'message':f"**AI**:'{answer}'"})
+                use_chat_box(False)
 
 if __name__ == "__main__":
     build_main_page()
